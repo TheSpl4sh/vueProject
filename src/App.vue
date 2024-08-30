5c23539f200e288dcb2fa94d70fb5cb1a0fd882d
TheSpl4sh <thesplash007@gmail.com>
2024-08-30 10:45:20 +0300

    feat: adding input and button "get weather"

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
       <header>
         <h1>Weather app</h1>
         <p>Find out the weather in your city</p>
+        <input type="text"  placeholder="Enter city">
+        <button>Get the weather</button>
       </header>
     </div>
 </template>
@@ -18,14 +20,48 @@
   height: 500px;
   border-radius: 50px;
   padding: 20px;
-  background-color: #171d60;
-  color: #a5a5a5;
+  background-color: #20223b;
+  color: #dddddd;
   text-align: center;
+}
+
+.wrapper p {
+  margin-top: 20px;
 }
 
 h1 {
   margin-top: 50px;
 }
 
+.wrapper input {
+  margin-top: 30px;
+  background-color: transparent;
+  border: 0;
+  border-bottom: 2px solid #20223b;
+  color: #dddddd;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+  transition: border-bottom-color 0.3s ease;
+}
+
+.wrapper input:focus {
+  border-bottom-color: white;
+}
+
+.wrapper button {
+  background-color: #42a232;
+  color: white;
+  border-radius: 10px;
+  border: 2px solid white;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translate(-5px);
+}
 
 </style>
