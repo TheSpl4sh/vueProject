2ab062c716156d684e9689b75193f173300e1952
TheSpl4sh <thesplash007@gmail.com>
2024-08-30 09:38:27 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+    
+</script>
+
+<template>
+    <div class="wrapper">
+      <header>
+        <h1>Weather app</h1>
+        <p>Find out the weather in your city</p>
+      </header>
+    </div>
+</template>
+
+
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: #171d60;
+  color: #a5a5a5;
+  text-align: center;
+}
+
+h1 {
+  margin-top: 50px;
+}
+
+
+</style>
